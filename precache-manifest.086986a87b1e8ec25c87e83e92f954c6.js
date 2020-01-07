e10e0e0df4bd8ad8a882a9d945d3bf5bf7cd0ed9
Hei-SenBerg <Hei-SenBerg@users.noreply.github.com>
2020-01-07 15:47:54 +0100

    Updates

---
+++ precache-manifest.086986a87b1e8ec25c87e83e92f954c6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4427a3c6677d3d940e2afa5a16f69f0c",
+    "url": "/Hei-SenBerg/dice_game/index.html"
+  },
+  {
+    "revision": "a413a3f84c1f18267520",
+    "url": "/Hei-SenBerg/dice_game/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "b72c4d28d1ac3de75ea3",
+    "url": "/Hei-SenBerg/dice_game/static/css/main.0d94c208.chunk.css"
+  },
+  {
+    "revision": "a413a3f84c1f18267520",
+    "url": "/Hei-SenBerg/dice_game/static/js/2.8f8c86f0.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Hei-SenBerg/dice_game/static/js/2.8f8c86f0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b72c4d28d1ac3de75ea3",
+    "url": "/Hei-SenBerg/dice_game/static/js/main.635cd31f.chunk.js"
+  },
+  {
+    "revision": "0ca9c026dfdfb0e6585a",
+    "url": "/Hei-SenBerg/dice_game/static/js/runtime-main.e5519ad3.js"
+  }
+]);
